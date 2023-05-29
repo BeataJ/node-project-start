6371a3ee3ffdc2e8dc796c1b248661a24bed951e
beataj <beata@jasinski.com>
2023-05-29 16:48:11 -0700

    routers

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const fs = require("fs");
 const path = require("path");
 
 const express = require("express");
-const uuid = require('uuid');
 
-const resData = require('./util/restaurant-data');
+const defaultRoutes = require('./routers/default');
+const restaurantRoutes = require('./routers/restaurants');
 
 const app = express();
 
@@ -14,71 +13,9 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.use('/', defaultRoutes);
+app.use("/", restaurantRoutes);
 
-  // const htmlFilePath = path.join(__dirname, "views", "index.html");
-  // res.sendFile(htmlFilePath);
-});
-
-app.get("/restaurants", (req, res) => {
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-
-  // const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html')
-  // res.sendFile(htmlFilePath)
-});
-
-app.get('/restaurants/:id', (req,res) => {
-  const restaurantId =  req.params.id;
-  const storedRestaurants = resData.getStoredRestaurants();
-
-  for (const restaurant of storedRestaurants) {
-    if(restaurant.id === restaurantId) {
-      return res.render("restaurants-detail", { restaurant: restaurant });
-    }
-  }
-
-  res.status(404).render('404');
-
-})
-
-app.get("/recommend", (req, res) => {
-  res.render("recommend");
-
-  // const htmlFildePath = path.join(__dirname, "views", "recommend.html");
-  // res.sendFile(htmlFildePath);
-});
-
-app.post("/recommend", (req, res) => {
-  const restaurant = req.body;
-  restaurant.id = uuid.v4();
-  const restaurants = resData.getStoredRestaurants();
-
-  restaurants.push(restaurant);
-
-  resData.storeRestaurants(restaurants);
-
-  res.redirect("confirm");
-});
-
-app.get("/confirm", (req, res) => {
-  res.render("confirm");
-
-  // const htmlFilePath = path.join(__dirname, "views", "confirm.html");
-  // res.sendFile(htmlFilePath);
-});
-
-app.get("/about", (req, res) => {
-  res.render("about");
-
-  // const htmlFilePath = path.join(__dirname, "views", "about.html");
-  // res.sendFile(htmlFilePath);
-});
 
 app.use((req,res) => {
   res.status(404).render('404');
